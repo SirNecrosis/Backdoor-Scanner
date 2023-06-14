a4bc86cac4da707f6c8660c6bcc4e7091dbe5333
V0idMatr1x <v0idmatr1x.0@gmail.com>
2023-06-14 14:08:39 -0500

    Patch: Refactor + Extend port support

--- scanner/options.go
+++ scanner/options.go
@@ -3,9 +3,42 @@ package scanner
 import (
 	"net"
 	"strconv"
-
+	
 	"github.com/SirNecrosis/Backdoor-Scanner/tools"
+	"github.com/SirNecrosis/Backdoor-Scanner/types"
 )
+
+func Conf() types.Config {
+	// Extend default port support
+	// ref: https://nmap.org/book/port-scanning.html#most-popular-ports
+	p := []int{
+		21, 
+		22, 
+		23, 
+		25, 
+		53,  
+		68,
+		80,
+		110, 
+		111,
+		135,
+		137, 
+		443,
+		445, 
+		514, 
+		631, 
+		993, 
+		1723, 
+		3306, 
+		3389, 
+		5900, 
+		8080,
+	}
+	return types.Config{
+		BackdoorPorts: p,
+		Protocol:      []string{"TCP", "UDP"},
+	}
+}
 
 func PortOpt(ip string, port int, protocol string) int {
 	var (
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,17 +1,6 @@
 package scanner
 
-import (
-	"fmt"
-
-	"github.com/SirNecrosis/Backdoor-Scanner/types"
-)
-
-func Conf() types.Config {
-	return types.Config{
-		BackdoorPorts: []int{21, 22, 23, 25, 53, 80, 443, 3306, 3389, 8080},
-		Protocol:      []string{"TCP", "UDP"},
-	}
-}
+import "fmt"
 
 func RunScan(ip string, port int, protocol string) {
 	if PortOpt(ip, port, protocol) == 0 {
